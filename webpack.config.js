bb94e6c93596977e66057ae1ed81deeb813a6a32
Alistair Chapman <alistair@agchapman.com>
2021-02-05 12:38:27 +1000

    Re-add pako as non-external dep; This removes the dependency on mod-report (for 1.4+); ..but it also dramatically increases the bundle size;

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 let webpack = require('vortex-api/bin/webpack').default;
 let config = webpack('vortex-showcase', __dirname, 4);
-config.externals["pako"] = "pako";
+// config.externals["pako"] = "pako";
 //this is effectively taking a hard dependency on nexus-mods/extension-mod-report
 // that extension is a) bundled and b) already includes pako
 // that's not ideal but doing it this way roughly halves the compiled extension size
